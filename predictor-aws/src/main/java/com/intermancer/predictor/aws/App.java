8139e4a3984dcdedaf0405938cdc4466a9674963
John Fryar <johnfryar@gmail.com>
2016-08-03 12:02:55 -0500

    Initial commit

---
+++ predictor-aws/src/main/java/com/intermancer/predictor/aws/App.java
@@ -0,0 +1,13 @@
+package com.intermancer.predictor.aws;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
